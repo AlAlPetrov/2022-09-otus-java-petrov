af58d9627000f3ec5fc60b765fae9a1c5813c699
Alexandr Petrov <alexandr_petrov_e@mail.ru>
2022-10-16 01:42:59 +0300

    feat: implemented CustomerService functionality

--- hw02-algorithms-and-containers/src/main/java/homework/CustomerService.java
+++ hw02-algorithms-and-containers/src/main/java/homework/CustomerService.java
@@ -1,23 +1,35 @@
 package homework;
 
-
+import java.util.AbstractMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class CustomerService {
+    TreeMap<Customer, String> Customers = new TreeMap<>();
 
-    //todo: 3. надо реализовать методы этого класса
-    //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    private Map.Entry<Customer, String> cloneEntry(Map.Entry<Customer, String> originalEntry) {
+        if (originalEntry == null) {
+            return null;
+        }
+
+        Customer customer = originalEntry.getKey();
+        return new AbstractMap.SimpleEntry<Customer, String>(
+                new Customer(customer.getId(),
+                        customer.getName(),
+                        customer.getScores()),
+                originalEntry.getValue());
+    }
 
     public Map.Entry<Customer, String> getSmallest() {
-        //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        return cloneEntry(Customers.firstEntry());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        return cloneEntry(Customers.higherEntry(customer));
     }
 
     public void add(Customer customer, String data) {
 
+        Customers.put(customer, data);
     }
 }
