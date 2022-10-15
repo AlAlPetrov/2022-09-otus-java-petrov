d5af8636f048826f944da8572e99c7a9f3cbdbd9
Alexandr Petrov <alexandr_petrov_e@mail.ru>
2022-10-15 23:02:28 +0300

    refactor: removed comments

--- hw02-algorithms-and-containers/src/main/java/homework/CustomerReverseOrder.java
+++ hw02-algorithms-and-containers/src/main/java/homework/CustomerReverseOrder.java
@@ -5,9 +5,6 @@ import java.util.LinkedList;
 
 public class CustomerReverseOrder {
 
-    //todo: 2. надо реализовать методы этого класса
-    //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
-
     LinkedList<Customer> Customers = new LinkedList<>();
     public void add(Customer customer) {
         Customers.addLast(customer);
