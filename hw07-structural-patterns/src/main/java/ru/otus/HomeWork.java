616ffc6676ffcb677ef522fca583348c19ac1a14
Alexandr Petrov <alexandr_petrov_e@mail.ru>
2022-11-28 00:04:58 +0300

    refactor: removed comments

--- hw07-structural-patterns/src/main/java/ru/otus/HomeWork.java
+++ hw07-structural-patterns/src/main/java/ru/otus/HomeWork.java
@@ -2,13 +2,6 @@ package ru.otus;
 
 public class HomeWork {
 
-    /*
-     Реализовать to do:
-       4. Сделать Listener для ведения истории (подумайте, как сделать, чтобы сообщения не портились)
-          Уже есть заготовка - класс HistoryListener, надо сделать его реализацию
-          Для него уже есть тест, убедитесь, что тест проходит
-     */
-
     public static void main(String[] args) {
         /*
            по аналогии с Demo.class
