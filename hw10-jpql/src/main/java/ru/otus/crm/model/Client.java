31ecd07afa017308e44e8313d7244acda1079bb7
Alexandr Petrov <alexandr_petrov_e@mail.ru>
2022-12-22 12:23:48 +0300

    feat: added models relations

--- hw10-jpql/src/main/java/ru/otus/crm/model/Client.java
+++ hw10-jpql/src/main/java/ru/otus/crm/model/Client.java
@@ -1,26 +1,37 @@
 package ru.otus.crm.model;
 
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
-@Getter
-@Setter
-@NoArgsConstructor
 @Entity
 @Table(name = "client")
+@Getter
+@NoArgsConstructor
+@Data
 public class Client implements Cloneable {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "idgen", initialValue = 1, allocationSize = 1, sequenceName = "client_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "idgen")
     @Column(name = "id")
     private Long id;
 
     @Column(name = "name")
     private String name;
+
+    @OneToOne(fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL)
+    @JoinColumn(name = "client")
+    private Address address;
+
+    @OneToMany(fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL)
+    @JoinColumn(name = "client", referencedColumnName = "id")
+    private List<Phone> phones;
 
     public Client(String name) {
         this.id = null;
@@ -32,9 +43,16 @@ public class Client implements Cloneable {
         this.name = name;
     }
 
+    public Client(Long id, String name, Address address, List<Phone> phones) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phones = phones;
+    }
+
     @Override
     public Client clone() {
-        return new Client(this.id, this.name);
+        return new Client(this.id, this.name, this.address, this.phones);
     }
 
     @Override
--- hw10-jpql/src/main/java/ru/otus/crm/model/Phone.java
+++ hw10-jpql/src/main/java/ru/otus/crm/model/Phone.java
@@ -1,5 +1,25 @@
 package ru.otus.crm.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "phone")
+@NoArgsConstructor
+@Data
 public class Phone {
+    @Id
+    @SequenceGenerator(name = "idgen", initialValue = 1, allocationSize = 1, sequenceName = "phone_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "idgen")
+    @Column(name = "id")
+    private Long id;
+
     private String number;
+
+    public Phone(Long id, String number) {
+        this.id = id;
+        this.number = number;
+    }
 }
