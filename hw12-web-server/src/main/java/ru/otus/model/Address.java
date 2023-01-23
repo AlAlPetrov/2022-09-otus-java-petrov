eb5277cf4d08446c8a78c46069acef29b12eecdd
AlAlPetrov <115040088+AlAlPetrov@users.noreply.github.com>
2023-01-23 17:59:17 +0300

    feat: implemented web server with authorization (#12)
    * feat: added homework template
    
    * feat: added data access layer
    
    * feat: implemented clients api
    
    * refactor: changed models location
    
    * refactor: fixed style issues
    
    * refactor: removed excessive generics
    
    * refactor: removed redirect status code from client servlet
    
    * test: fixed server instantiation
    
    * refactor: remove unused code

---
+++ hw12-web-server/src/main/java/ru/otus/model/Address.java
@@ -0,0 +1,25 @@
+package ru.otus.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "address")
+@NoArgsConstructor
+@Data
+public class Address {
+    @Id
+    @SequenceGenerator(name = "idgen", initialValue = 1, allocationSize = 1, sequenceName = "address_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "idgen")
+    @Column(name = "id")
+    private Long id;
+
+    private String street;
+
+    public Address(Long id, String street) {
+        this.id = id;
+        this.street = street;
+    }
+}
