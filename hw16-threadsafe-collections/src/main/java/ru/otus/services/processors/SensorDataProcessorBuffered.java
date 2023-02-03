aba199ec787ed233d4cdc5f5c9307edb8c55a2c5
Alexandr Petrov <alexandr_petrov_e@mail.ru>
2023-02-03 23:48:15 +0300

    feat: implemented buffered data processor

--- hw16-threadsafe-collections/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
+++ hw16-threadsafe-collections/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -2,14 +2,26 @@ package ru.otus.services.processors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.otus.lib.SensorDataBufferedWriter;
 import ru.otus.api.SensorDataProcessor;
 import ru.otus.api.model.SensorData;
+import ru.otus.lib.SensorDataBufferedWriter;
 
-// Этот класс нужно реализовать
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.concurrent.PriorityBlockingQueue;
+
+class MeasurementTimeComparator implements Comparator<SensorData> {
+    public int compare(SensorData left, SensorData right)
+    {
+        return left.getMeasurementTime().compareTo(right.getMeasurementTime());
+    }
+}
 public class SensorDataProcessorBuffered implements SensorDataProcessor {
     private static final Logger log = LoggerFactory.getLogger(SensorDataProcessorBuffered.class);
 
+    private final int defaultInitialCapacity = 11;
+    private PriorityBlockingQueue<SensorData> dataBuffer = new PriorityBlockingQueue<>(defaultInitialCapacity,
+            new MeasurementTimeComparator());
     private final int bufferSize;
     private final SensorDataBufferedWriter writer;
 
@@ -20,16 +32,18 @@ public class SensorDataProcessorBuffered implements SensorDataProcessor {
 
     @Override
     public void process(SensorData data) {
-    /*
+        dataBuffer.put(data);
         if (dataBuffer.size() >= bufferSize) {
             flush();
         }
-    */
     }
 
     public void flush() {
         try {
-            //writer.writeBufferedData(bufferedData);
+            var outputBuffer = new ArrayList<SensorData>();
+            if (dataBuffer.drainTo(outputBuffer, bufferSize) > 0) {
+                writer.writeBufferedData(outputBuffer);
+            }
         } catch (Exception e) {
             log.error("Ошибка в процессе записи буфера", e);
         }
