3f6d7f25cc530de6efcad8a09f6f2e70e131aa48
Alexandr Petrov <alexandr_petrov_e@mail.ru>
2023-02-12 23:59:52 +0300

    feat: implemented grpc server

--- hw17-grpc-client-server-application/src/main/java/ru/otus/protobuf/GRPCServer.java
+++ hw17-grpc-client-server-application/src/main/java/ru/otus/protobuf/GRPCServer.java
@@ -1,11 +1,8 @@
 package ru.otus.protobuf;
 
 
-import io.grpc.Server;
 import io.grpc.ServerBuilder;
-import ru.otus.protobuf.service.RealDBService;
-import ru.otus.protobuf.service.RealDBServiceImpl;
-import ru.otus.protobuf.service.RemoteDBServiceImpl;
+import ru.otus.protobuf.service.SequentialNumbersService;
 
 import java.io.IOException;
 
@@ -14,15 +11,16 @@ public class GRPCServer {
     public static final int SERVER_PORT = 8190;
 
     public static void main(String[] args) throws IOException, InterruptedException {
-
-        var dbService = new RealDBServiceImpl();
-        var remoteDBService = new RemoteDBServiceImpl(dbService);
-
         var server = ServerBuilder
                 .forPort(SERVER_PORT)
-                .addService(remoteDBService).build();
+                .addService(new SequentialNumbersService()).build();
         server.start();
         System.out.println("server waiting for client connections...");
+
+        Runtime.getRuntime().addShutdownHook( new Thread( () -> {
+            server.shutdown();
+        }));
+
         server.awaitTermination();
     }
 }
