bf2dbf8fcf981ac5cbf9406e51a5da0a7164ad40
AlAlPetrov <115040088+AlAlPetrov@users.noreply.github.com>
2023-02-12 19:40:44 +0300

    feat: implemented buffered sensor data processor (#15)
    * feat: implemented homework template
    
    * feat: implemented buffered data processor
    
    * refactor: added blank line to separate classes
    
    * perf: initial buffer size is set to requested buffer size value

---
+++ hw16-threadsafe-collections/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -0,0 +1,56 @@
+package ru.otus.services.processors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.api.SensorDataProcessor;
+import ru.otus.api.model.SensorData;
+import ru.otus.lib.SensorDataBufferedWriter;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.concurrent.PriorityBlockingQueue;
+
+class MeasurementTimeComparator implements Comparator<SensorData> {
+    public int compare(SensorData left, SensorData right)
+    {
+        return left.getMeasurementTime().compareTo(right.getMeasurementTime());
+    }
+}
+
+public class SensorDataProcessorBuffered implements SensorDataProcessor {
+    private static final Logger log = LoggerFactory.getLogger(SensorDataProcessorBuffered.class);
+    private final int bufferSize;
+    private final SensorDataBufferedWriter writer;
+    private final  PriorityBlockingQueue<SensorData> dataBuffer;
+
+    public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
+        this.bufferSize = bufferSize;
+        this.writer = writer;
+        this.dataBuffer  = new PriorityBlockingQueue<>(bufferSize,
+                                                new MeasurementTimeComparator());
+    }
+
+    @Override
+    public void process(SensorData data) {
+        dataBuffer.put(data);
+        if (dataBuffer.size() >= bufferSize) {
+            flush();
+        }
+    }
+
+    public void flush() {
+        try {
+            var outputBuffer = new ArrayList<SensorData>();
+            if (dataBuffer.drainTo(outputBuffer, bufferSize) > 0) {
+                writer.writeBufferedData(outputBuffer);
+            }
+        } catch (Exception e) {
+            log.error("Ошибка в процессе записи буфера", e);
+        }
+    }
+
+    @Override
+    public void onProcessingEnd() {
+        flush();
+    }
+}
