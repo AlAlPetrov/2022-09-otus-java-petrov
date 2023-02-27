b1c686d91aa3ef80ca8cfb7276221d6ca58ba9cb
Alexandr Petrov <alexandr_petrov_e@mail.ru>
2023-02-27 12:09:24 +0300

    feat: added homework template

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -1,11 +1,11 @@
 public interface Versions {
-    String guava = "31.0.1-jre";
+    String guava = "31.1-jre";
     String jmh = "1.35";
     String asm = "9.2";
     String glassfishJson = "2.0.1";
 
     String lombok = "1.18.24";
-    String gson = "2.9.1";
+    String gson = "2.10.1";
 
     String mongodb = "4.7.1";
     String mongodbReactive = "4.7.1";
@@ -27,4 +27,6 @@ public interface Versions {
     String errorProneAnnotations = "2.11.0";
     String tomcatAnnotationsApi = "6.0.53";
     String grpc = "1.44.1";
+    String wiremock = "3.0.0-beta-2";
+    String r2dbcPostgresql = "0.8.13.RELEASE";
 }
